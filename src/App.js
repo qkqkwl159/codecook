533c59e4b030030dc7d88f0c6dc5046bd173dac2
qkqkwl159 <qkqkwl147@gmail.com>
2022-07-07 18:40:50 +0900

    mac0707,1840

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello!</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Home.css';
+import myimg from './my_profile.jpg';
+
+
+
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className='home_container'>
+                <div className='home_profile'>
+                    <h1>MY_PROFILE__</h1>
+                    <img
+                    className="home_img"
+                    src={myimg}
+                    alt=''/>
+                    <br></br>
+                    <div className='profile_name'>Kim HuiJun</div>
+                    <br></br>
+                    <span className='profile_major'>💻 Major : Computer Engineering</span>
+                    <br></br>
+                    <div className="profile_introduce">
+                        I am a third year student in computer engineering<br/>
+                        I want to experience many things and make them.<br/>
+                        I'll put the records here and Passion moves me<br/>
+                    </div>
+
+                    
+                </div>
+
+            
+            </div>
+        </div>
+    );
+}
+
+export default Home;
